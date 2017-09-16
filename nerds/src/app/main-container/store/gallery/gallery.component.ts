c3d9c430ec10344d8881e1dd438d55437cab0a45
Vitaliy <vitaliy@altsolution.net>
2017-09-16 19:56:41 +0300

    show gallery items

--- nerds/src/app/main-container/store/gallery/gallery.component.ts
+++ nerds/src/app/main-container/store/gallery/gallery.component.ts
@@ -12,4 +12,64 @@ export class GalleryComponent implements OnInit {
   ngOnInit() {
   }
 
+  public links: GalleryItem[] = LINKS;
+
 }
+
+export class GalleryItem {
+  imgSrc: string;
+  name: string;
+  title: string;
+  price: string;
+}
+
+let LINKS: GalleryItem[] = [
+  {
+    imgSrc: './assets/img/galery-1.png',
+    name: 'Aviator',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '10 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-2.png',
+    name: 'Sunset',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '20 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-3.png',
+    name: 'Forte',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '30 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-4.png',
+    name: 'Marquee',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '40 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-5.png',
+    name: 'Avenue',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '50 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-6.png',
+    name: 'Dovetail',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '60 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-1.png',
+    name: 'Aviator',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '10 000 p.'
+  },
+  {
+    imgSrc: './assets/img/galery-2.png',
+    name: 'Sunset',
+    title: 'Интернет-магазин с личным кабинетом',
+    price: '20 000 p.'
+  }
+]
