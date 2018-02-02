b299eb53c87bd1fa8b9fc062b85585e65f3cf2e5
Vitaliy <vitaliy@altsolution.net>
2018-02-02 13:46:57 +0200

    auth

--- ngcalc/src/app/auth/login/login.component.ts
+++ ngcalc/src/app/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { UsersService } from '../../shared/services/users.service';
+import { User } from '../../shared/models/user.model';
+import { Message } from '../../shared/models/message.model';
 
 @Component({
   selector: 'vp-login',
@@ -10,18 +13,44 @@ import { Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  message: Message;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.message = new Message('danger', '');
+
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required,Validators.minLength(6)])
     });
   }
 
+  showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
+
   onSubmit() {
-    console.log(this.form);
+    const formData = this.form.value;
+   
+
+    this.usersService.getUserByEmail(formData.email)
+      .subscribe((user: User) => {
+        if(user) {
+          if(user.password === formData.password) {
+
+          } else {
+            this.showMessage('Неправильный пароль')
+          }
+        } else {
+          this.showMessage('Неправильный логин')
+        }
+      })
   }
 
 }
